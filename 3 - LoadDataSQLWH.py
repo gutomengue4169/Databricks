5cd7718ab3ebea4829f575db5c699915a92332ab
gutomengue4169 <57238218+gutomengue4169@users.noreply.github.com>
2021-03-10 22:17:13 -0300

    Save File

---
+++ 3 - LoadDataSQLWH.py
@@ -0,0 +1,35 @@
+# Databricks notebook source
+from pyspark.sql import SparkSession
+from pyspark.sql import functions as F
+
+# get the conn with SQL server
+jdbchost = "devserverescola.database.windows.net"
+jdbcport = "1433"
+jdbcdatabase = "dev_database_escola"
+properties = {
+    "user": "sa-dev",
+    "password": "Password@1"
+}
+
+url = "jdbc:sqlserver://{0}:{1};database={2}".format(jdbchost,jdbcport,jdbcdatabase)
+
+# COMMAND ----------
+
+#create a view from spark table
+dfs = spark.sql("select * from registroenderecoescola")
+
+# COMMAND ----------
+
+# create a view from target table 
+dft = spark.read.jdbc(url=url, table="registroenderecoescola", properties=properties)
+
+# COMMAND ----------
+
+left_join = dfs.join(dft, (dfs._id == dft._id) & (dfs.nome==dft.nome) & (dfs.DateExtraction==dft.DateExtraction) ,how='leftanti')
+
+# COMMAND ----------
+
+left_join.write.jdbc( url=url, table= "registroenderecoescola", mode= "append", properties=properties)
+
+# COMMAND ----------
+
